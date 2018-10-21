80d4df5f6e7ddb969277ecd338e9600beb0c4c5d
kraussg <gabby.krauss@gmail.com>
2018-10-21 14:27:02 -0400

    3rd Commit

--- randomPic/randompic.js
+++ randomPic/randompic.js
@@ -1,15 +1,6 @@
-/*
-function randomPic() {
-    var pics = new Array();
-    var pics = ["img/bella.jpg","img/bella2.jpg", "img/huxley.jpg"];
-    var picChoice = Math.floor(Math.random()*pics.length);
-    showpic = pics[picChoice];
-    document.body.style.backgroundImage = "url('"+ showpic+"')";
-   // document.body.style.backgroundSize= "cover";
-    //document.body.style.backgroundAttachment = "fixed";
-}*/
 
+window.onload = pic;
 var pics = ["imgs/bella.jpeg","imgs/bella2.jpg","imgs/huckley.jpg"]
-var number = Math.floor(Math.random()*pic.length);
+var randomPic = Math.floor(Math.random()*pic.length);
 var pic = pics[number];
-document.getElementById("wrapper").style.backgroundImage = "url('"+pic+"')";
+document.getElementById("randomPic").src = pics[randomPic];
